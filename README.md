397abe1b310debb0be3e3bf64c8b4d5f2816b394
Shanto Islam <rajs45693@gmail.com>
2023-08-07 16:59:18 +0600

    iimplement layout and home components

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# React + Vite
+# Online video editing platform
 
 This template provides a minimal setup to get React working in Vite with HMR and some ESLint rules.
 
---
+++ src/Components/LayOut/LayOut.jsx
@@ -0,0 +1,12 @@
+import { Outlet } from "react-router-dom";
+
+
+const LayOut = () => {
+    return (
+        <div>
+            <Outlet/>
+        </div>
+    );
+};
+
+export default LayOut;
