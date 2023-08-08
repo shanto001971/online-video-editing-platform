e21b1a66a12e7df654c4471c3d9a3750f88f06a4
TobiborRahman <r.tobibor16@gmail.com>
2023-08-09 00:47:30 +0600

    video layout section added

--- src/Components/Pages/VideoLayout/VideoLayout.jsx
+++ src/Components/Pages/VideoLayout/VideoLayout.jsx
@@ -1,10 +1,10 @@
-import img from '../../../public/Team.png';
-import video from '../../../public/Fluid_Animations_86db7504d9.mp4';
+import img from '../../../../public/Team.png';
+import video from '../../../../public/Fluid_Animations_86db7504d9.mp4';
 
 const VideoLayout = () => {
 	return (
-		<div className="md:flex w-[100%] h-[100vh]">
-			<div className="w-[500px] border my-10 ml-10 p-8 bg-gray-100 rounded-3xl">
+		<div className="md:flex h-[100vh] my-10">
+			<div className="border mr-10 p-8 bg-gray-100 rounded-3xl">
 				<h2 className="text-3xl font-bold">
 					Add your team and collaborate on your video
 				</h2>
@@ -14,11 +14,19 @@ const VideoLayout = () => {
 					your team members.
 				</h4>
 			</div>
-			<div className="border m-10 bg-blue-900 rounded-3xl">
+			<div className="border bg-blue-300 rounded-3xl">
 				<h2 className="text-3xl font-bold p-8">
 					Attractive Animations
 				</h2>
-				<video autoPlay loop className="h-[300px] w-full">
+
+				<video
+					autoPlay
+					loop
+					muted
+					playsInline
+					preload="auto"
+					className="h-[300px] w-[100vw]"
+				>
 					<source className="w-full" src={video} type="video/mp4" />
 				</video>
 			</div>
@@ -27,31 +35,3 @@ const VideoLayout = () => {
 };
 
 export default VideoLayout;
-
-// import img from '../../../public/Team.png';
-// import video from '../../../public/Fluid_Animations_86db7504d9.mp4';
-
-// const HomeLayout = () => {
-// 	return (
-// 		<div className="md:flex w-[100%] h-[100vh]">
-// 			<div className="w-[500px] border my-10 ml-10 p-8 bg-gray-100 rounded-3xl">
-// 				<h2 className="text-3xl font-bold">
-// 					Add your team and collaborate on your video
-// 				</h2>
-// 				<img className="my-16" src={img} alt="" />
-// 				<h4>
-// 					Create, edit, comment, and share your videos together with
-// 					your team members.
-// 				</h4>
-// 			</div>
-// 			<div className="border m-10 bg-blue-900 rounded-3xl">
-// 				<h2 className="text-3xl font-bold p-8">
-// 					Attractive Animations
-// 				</h2>
-// 				<video autoPlay loop className="h-[300px] w-full">
-// 					<source className="w-full" src={video} type="video/mp4" />
-// 				</video>
-// 			</div>
-// 		</div>
-// 	);
-// };
