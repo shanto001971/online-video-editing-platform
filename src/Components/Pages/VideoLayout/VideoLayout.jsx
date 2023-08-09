dbb958e4e419a4bc532f9003fb118b7df4b54506
TobiborRahman <r.tobibor16@gmail.com>
2023-08-09 10:50:41 +0600

    margin padding added

--- src/Components/Pages/VideoLayout/VideoLayout.jsx
+++ src/Components/Pages/VideoLayout/VideoLayout.jsx
@@ -4,7 +4,7 @@ import video from '../../../../public/Fluid_Animations_86db7504d9.mp4';
 const VideoLayout = () => {
 	return (
 		<div className="md:flex md:h-[100vh] my-10">
-			<div className="w-full md:w-auto border p-8 mr-10 mb-5 md:mb-0 bg-gray-100 rounded-3xl ">
+			<div className="md:w-auto border p-8 mr-8 mb-5 md:mb-0 bg-gray-100 rounded-3xl ">
 				<h2 className="text-3xl font-bold">
 					Add your team and collaborate on your video
 				</h2>
@@ -25,11 +25,11 @@ const VideoLayout = () => {
 					muted
 					playsInline
 					preload="auto"
-					className=" md:h-[300px] w-[100vw] rounded-3xl"
+					className=" md:h-[300px] w-[100vw]"
 				>
 					<source className="w-full" src={video} type="video/mp4" />
 				</video>
-				<ul className="flex gap-x-3 md:text-2xl font-bold py-10 pl-5 md:pl-10">
+				<ul className="flex gap-x-3 md:text-2xl text-gray-400 font-semibold py-10 pl-5 md:pl-10">
 					<li>Slide</li>
 					<li>Blur</li>
 					<li>Bounce</li>
