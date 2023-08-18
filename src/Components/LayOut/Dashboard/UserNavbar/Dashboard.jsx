664c88309036b4c83086714b83db014bd2ef1b2a
Shanto Islam <rajs45693@gmail.com>
2023-08-18 17:20:29 +0600

    implement some issu

--- src/Components/LayOut/Dashboard/UserNavbar/Dashboard.jsx
+++ src/Components/LayOut/Dashboard/UserNavbar/Dashboard.jsx
@@ -1,12 +1,12 @@
 import { useContext } from "react";
 import { Link, NavLink } from "react-router-dom";
 import logoCopy from "../../../../assets/logoCopy.png";
-import {FiCornerUpLeft, FiCornerUpRight} from "react-icons/fi";
+import { FiCornerUpLeft, FiCornerUpRight } from "react-icons/fi";
 import UserModal from "./UserModal";
 import CreateModal from "./CreateModal";
 import { AuthContext } from "../../../../providers/AuthProvider";
 const Dashboard = () => {
-  const {user}= useContext(AuthContext);
+  const { user } = useContext(AuthContext);
   return (
     <div className=" text-gray-500 font-medium  md:py-1  sm:max-w-xs md:max-w-screen-md lg:max-w-screen-xl  ">
       <div className="relative flex items-center justify-between">
@@ -15,9 +15,10 @@ const Dashboard = () => {
           <Link to="/" title="Home">
             <img src={logoCopy} alt="logo" className=" w-16 rounded-full" />
           </Link>
-Projects        </div>
-<ul className="items-center hidden space-x-4 lg:flex">
-         
+          Projects
+        </div>
+        <ul className="items-center hidden space-x-4 lg:flex">
+
           <li>
             <NavLink
               to="/dashboard"
@@ -39,13 +40,13 @@ Projects        </div>
           </li>
         </ul>
         <ul className="items-center  lg:space-x-3 flex  text-gray-400 font-semibold text-xl">
-         
-<FiCornerUpLeft className="text-gray-500"/>          
-<FiCornerUpRight className="text-gray-500"/>   
-<CreateModal/>       
-         <UserModal user={user}/>
+
+          <FiCornerUpLeft className="text-gray-500" />
+          <FiCornerUpRight className="text-gray-500" />
+          <CreateModal />
+          <UserModal user={user} />
         </ul>
-       
+
       </div>
     </div>
   );
